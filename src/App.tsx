c5096c0aa8676eb877e436c7b3acae41f4886a64
emaliikhokhar <bitf18a024@pucit.edu.pk>
2021-10-05 16:52:16 +0500

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface IProps {
+  intro?: string
+}
+
+const Welcome = (props: IProps) => {
+  return <>{props.intro}</>
+}
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome intro="Welcome to the landing page"/>
       </header>
     </div>
   );
