b2f69bf3db74adba047bd434e8ea150776fee6d5
emaliikhokhar <bitf18a024@pucit.edu.pk>
2021-10-05 16:52:45 +0500

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css';
 
-interface IProps {
-  intro?: string
-}
-
-const Welcome = (props: IProps) => {
-  return <>{props.intro}</>
-}
-
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <Welcome intro="Welcome to the landing page"/>
       </header>
     </div>
   );
