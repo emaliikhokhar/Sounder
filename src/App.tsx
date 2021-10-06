a160091bf5e98fe25be8fd614a8b6396cb40689a
emaliikhokhar <bitf18a024@pucit.edu.pk>
2021-10-06 12:28:51 +0500

    Basic: Done

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,59 @@ import Header from './Components/Header/Header';
 import Player from './Components/Player/Player';
 
 function App() {
-  let tracks = {
-    1: {
+  let tracks = [
+    {
+      id: 1,
       name: "Mustafa Jaan-e-Rehmat",
       artist: "Atif Aslam",
     },
-    2: {
+    {
+      id: 2,
       name: "Ae Saba Mustafa",
       artist: "Rafaqat Ali Khan",
     },
-    3: {
+    {
+      id: 3,
       name: "Mera Dil Badal De",
       artist: "Junaid Jamshed",
     },
-    4: {
+    {
+      id: 4,
       name: "Sehar Ka Waqt Tha",
       artist: "Arbaaz",
     }
+  ]
+
+  const [playlist] = useState(tracks);
+  const [sound, setSound] = useState(playlist[0])
+
+  const moveForward = (id: number) => {
+    console.log("clicked")
+    if (id === tracks.length) {
+      setSound(playlist[0])
+      console.log(playlist[0])
+    }
+    else {
+      setSound(playlist[id+1])
+      console.log(playlist[id+1])
+    }
   }
 
-  const [playlist, setPlaylist] = useState(tracks);
+  const moveBackward = (id: number) => {
+    if (id === tracks.length) {
+      setSound(playlist[tracks.length])
+      console.log(playlist[tracks.length])
+    }
+    else {
+      setSound(playlist[id-1])
+      console.log(playlist[id-1])
+    }
+  }
 
   return (
     <div className="App-header">
       <Header heading="Sounder App"/>
-      <Player playlist={playlist}/>
+      <Player playlist={playlist} soundActive={sound} moveForward={moveForward} moveBackward={moveBackward}/>
       <Footer outro="Powered by " poweredBy = "Ali A. Khokhar"/>
     </div>
   );
--- src/Components/Player/Player.tsx
+++ src/Components/Player/Player.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import "./Player.css"
 
 interface IProps {
-    playlist: Object 
+    playlist: Object,
+    soundActive: Object,
+    moveForward: (id: number) => void,
+    moveBackward: (id: number) => void
 }
 
 const Player = (props: IProps) => {
     return (
         <div className="bodyDiv">
-            <img src={require("../../Images/Ae Saba Mustafa.jpg")}/>
+            <button onClick={() => props.moveBackward(2)}>Down</button>
+            <button onClick={() => props.moveForward(0)}>Up</button>
         </div>
     )
 }
